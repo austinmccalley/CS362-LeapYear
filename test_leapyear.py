65b1db8b7da17f5b7b9e072a595c124019b45194
Austin McCalley <austin@austinmccalley.com>
2021-05-20 16:20:49 -0700

    Finish tests before implementation

---
+++ test_leapyear.py
@@ -0,0 +1,28 @@
+import unittest
+
+
+class test_LeapYear(unittest.TestCase):
+
+  def test2020(self):
+    self.assertTrue(leapYear('2020'))
+
+  def test2019(self):
+    self.assertFalse(leapYear('2019'))
+
+  def testEdge(self):
+    self.assertTrue(leapYear('0'))
+
+  def testNonYear(self):
+    self.assertFalse(leapYear('abc'))
+
+  def test2000(self):
+    self.assertTrue(leapYear('2000'))
+
+  def test1972(self):
+    self.assertTrue(leapYear('1972'))
+
+  def test1974(self):
+    self.assertFalse(leapYear('1974'))
+
+if __name__ == '__main__':
+  unittest.main()
